aaf485d6d7aeba89cd0a3839f3316940e5a38491
black32h <blackspetya@gmail.com>
2025-02-23 19:47:22 +0200

    Update CurrencyRateRepository.java

--- src/main/java/com/example/currencyaggregator/repository/CurrencyRateRepository.java
+++ src/main/java/com/example/currencyaggregator/repository/CurrencyRateRepository.java
@@ -1,4 +1,15 @@
 package com.example.currencyaggregator.repository;
 
-public class CurrencyRateRepository {
+import com.example.currencyaggregator.model.CurrencyRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyRateRepository extends JpaRepository<CurrencyRate, Long> {
+
+    // JpaRepository вже надає методи для роботи з таблицею CurrencyRate:
+    //  - findById(id): шукає курс валют за id.
+    //  - save(entity): зберігає або оновлює курс валют.
+    //  - delete(entity): видаляє курс валют.
+    //  - та інші стандартні методи для роботи з базою даних.
 }
