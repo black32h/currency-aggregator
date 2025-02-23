6db9f484b06b7f50efaa84bde2b3283ece75bf18
black32h <blackspetya@gmail.com>
2025-02-23 19:47:52 +0200

    Update CurrencyAggregatorApplication.java

--- src/main/java/com/example/currencyaggregator/CurrencyAggregatorApplication.java
+++ src/main/java/com/example/currencyaggregator/CurrencyAggregatorApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurrencyAggregatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyAggregatorApplication.class, args);
-	}
-
+    // Головний метод для запуску Spring Boot додатку
+    public static void main(String[] args) {
+        // Запуск додатку за допомогою SpringApplication.run
+        SpringApplication.run(CurrencyAggregatorApplication.class, args);
+    }
 }
+//http://localhost:8081/api/exchange-rates
